bea76ea5a4d4843669acf1b8482a557a7b0774d9
jquiroz <jquiroz2983@gmail.com>
2022-12-07 23:03:47 -0500

    07-dic-5

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM node
 WORKDIR /app
 COPY package.json .
 RUN npm i
-COPY . .
+COPY /dist .
 ## EXPOSE [Port you mentioned in the vite.config file]
 EXPOSE 5173
 CMD ["npm", "run", "dev"]
--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/vite-deploy-demo/'
+  base: '/vite-deploy-demo/',
+  server: {
+    watch: {
+      usePolling: true,
+    },
+    host: true, // needed for the Docker Container port mapping to work
+    strictPort: true,
+    port: 5173, // you can replace this port with any port
+  }
 })
